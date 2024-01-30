026cebae73ff8dbdff91ffccba51214cd11202e1
kbilinski <kajetan.bilinski@neosystem.pl>
2024-01-30 02:51:36 +0100

    Clear

--- Database/dbo/Tables/Oferta.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Oferta] (
-    [idOferta]      INT           NOT NULL,
-    [Nazwa]         VARCHAR (150) NOT NULL,
-    [Opis]          VARCHAR (500) NOT NULL,
-    [idUslugodawca] INT           NOT NULL,
-    CONSTRAINT [Oferta_pk] PRIMARY KEY CLUSTERED ([idOferta] ASC),
-    CONSTRAINT [Oferta_Uslugodawca] FOREIGN KEY ([idUslugodawca]) REFERENCES [dbo].[Uslugodawca] ([idUslugodawca])
-);
-
--- Database/dbo/Tables/Uslugobiorca.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Uslugobiorca] (
-    [idUslugobiorca] INT          NOT NULL,
-    [Imie]           VARCHAR (50) NOT NULL,
-    [Nazwisko]       VARCHAR (50) NOT NULL,
-    CONSTRAINT [Uslugobiorca_pk] PRIMARY KEY CLUSTERED ([idUslugobiorca] ASC)
-);
-
